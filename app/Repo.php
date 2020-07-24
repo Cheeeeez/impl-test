ab2685812d215d06194886c87033453a807fe965
Nam <nguyentrongnama8@gmail.com>
2020-07-24 13:35:21 +0700

    add column user_id to repo table

--- app/Repo.php
+++ app/Repo.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Repo extends Model
 {
     protected $timestamp = false;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'url',
+        'user_id',
+    ];
 }
