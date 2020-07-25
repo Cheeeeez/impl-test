b43707cedccb7baefb7d5169cd2c9da06b56a9fa
Nam <nguyentrongnama8@gmail.com>
2020-07-25 18:13:07 +0700

    delete form in repo.fork.blade

--- resources/views/repo/fork.blade.php
+++ resources/views/repo/fork.blade.php
@@ -2,7 +2,6 @@
 @section('slide-content')
 <input type="hidden" value="{{$id}}" id="id">
 <input type="hidden" value="{{Auth::user()->token}}" id="token">
-<form action=""></form>
 @endsection
 @section('script')
 <script src="{{ asset('js/fork.js') }}"></script>
