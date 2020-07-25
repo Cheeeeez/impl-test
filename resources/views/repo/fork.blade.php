66d6397f40eec6f31cabf955c937d43e478db0f2
Nam <nguyentrongnama8@gmail.com>
2020-07-25 15:43:19 +0700

    add ajax jquery to fork.blade

--- resources/views/repo/fork.blade.php
+++ resources/views/repo/fork.blade.php
@@ -1,9 +1,11 @@
 @extends('home.master')
 @section('slide-content')
 <input type="hidden" value="{{$id}}" id="id">
+<input type="hidden" value="{{Auth::user()->token}}" id="token">
 @endsection
 @section('script')
 <script>
+    let token1 = $("#token").val();
     $(document).ready(function() {
         $.ajax({
             type: "GET",
@@ -12,11 +14,11 @@
             success: function (response) {
                 console.log(response)
                 $.ajax({
-                    type: "POST",
-                    url: response.forks_url,
-                    // dataType: "dataType",
+                    type: "GET",
+                    url: "https://github.com/login/oauth/authorize",
+                    dataType: "JSON",
                     success: function (result) {
-                        console.result
+                        console.log(result)
                     }
                 });
             }
