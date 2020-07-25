7618c478e8621152a164cf99456d17ead2288fc9
Nam <nguyentrongnama8@gmail.com>
2020-07-25 10:45:28 +0700

    add method fork to RepoController

--- app/Http/Controllers/RepoController.php
+++ app/Http/Controllers/RepoController.php
@@ -15,14 +15,21 @@ class RepoController extends Controller
 
     public function store(Request $request)
     {
-        Repo::create($request->all());
-        session()->flash('success', 'Clone successfully');
-        return redirect()->route('home');
+        $repo = Repo::create($request->all());
+        $id = $repo->id;
+        session()->flash('success', "Clone repository with ID $id successfully");
+        return redirect()->route('repo.index');
     }
 
     public function index()
     {
         $repos = Repo::all();
         return view('repo.list', compact('repos'));
+    }
+
+    public function fork(Request $request)
+    {
+        $id = $request->id;
+        return view('repo.fork', compact('id'));
     }
 }
