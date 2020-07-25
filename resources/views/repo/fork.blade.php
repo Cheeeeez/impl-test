631788db73567086b68ce62d06ab53b853d846d8
Nam <nguyentrongnama8@gmail.com>
2020-07-25 18:11:47 +0700

    add file fork.js

--- resources/views/repo/fork.blade.php
+++ resources/views/repo/fork.blade.php
@@ -5,34 +5,5 @@
 <form action=""></form>
 @endsection
 @section('script')
-<script>
-    let id = $("#id").val();
-    let accessToken = $("#token").val();
-    $(document).ready(function() {
-        $.ajax({
-            type: "GET",
-            url: "https://api.github.com/repositories/" + id,
-            dataType: "JSON",
-            success: function (response) {
-                $.ajax({
-                    type: "POST",
-                    url: response.forks_url,
-                    dataType: "JSON",
-                    headers: {Authorization : "token " + accessToken},
-                    success: function (result) {
-                        $.ajax({
-                            type: "POST",
-                            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                            url: "/" + id + "/update",
-                            data: {forkedUrl: result.html_url},
-                            success: function (outcome) {
-                                window.location.href = "/repo-list";
-                            }
-                        });
-                    }
-                });
-            }
-        });
-    })
-</script>
+<script src="{{ asset('js/fork.js') }}"></script>
 @endsection
